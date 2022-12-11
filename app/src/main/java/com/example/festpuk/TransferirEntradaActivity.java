17efd2f4fe13d7b903f0d676de6b9e232ffababa
Víctor Díaz <diaz.victorm@pucp.edu.pe>
2022-12-10 20:42:32 -0500

    vistas organizador aaaaaaaaaa

---
+++ app/src/main/java/com/example/festpuk/TransferirEntradaActivity.java
@@ -0,0 +1,14 @@
+package com.example.festpuk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TransferirEntradaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transferir_entrada);
+    }
+}
---
+++ app/src/main/java/com/example/festpuk/organizador/EventoCanceladoActivity.java
@@ -0,0 +1,15 @@
+package com.example.festpuk.organizador;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.festpuk.R;
+
+public class EventoCanceladoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evento_cancelado);
+    }
+}
