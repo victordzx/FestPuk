ab28977fcce316ea70a93b4199988632827bf126
Víctor Díaz <diaz.victorm@pucp.edu.pe>
2022-12-11 17:00:49 -0500

    evento foto

--- app/src/main/java/com/example/festpuk/ValidacionEventoActivity.java
+++ app/src/main/java/com/example/festpuk/ValidacionEventoActivity.java
@@ -1,6 +1,7 @@
 package com.example.festpuk;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.Manifest;
 import android.content.Intent;
@@ -28,28 +29,5 @@ public class ValidacionEventoActivity extends AppCompatActivity {
 
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent,CAMARA_CODE);
-
-        binding.btnEnviarFoto.setOnClickListener(v->{
-            goMain();
-        });
-    }
-
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if(requestCode==PERMISOS_CODE && grantResults.length > 0){
-            if (grantResults[0] != PackageManager.PERMISSION_GRANTED){
-                // Se piden los permisos nuevamente
-                if (checkSelfPermission(Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
-                    requestPermissions(new String[]{Manifest.permission.CAMERA},PERMISOS_CODE);
-                }
-            }
-        }
-    }
-
-    private void goMain(){
-        Intent intent = new Intent(this, OrganizadorMainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
     }
 }
