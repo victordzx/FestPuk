7196cfa94ee3e5fb9894d64871bc82378e7e4edf
Víctor Díaz <diaz.victorm@pucp.edu.pe>
2022-12-11 11:44:17 -0500

    vista perfil con fallas

--- app/src/main/java/com/example/festpuk/cliente/ClientePerfilFragment.java
+++ app/src/main/java/com/example/festpuk/cliente/ClientePerfilFragment.java
@@ -2,65 +2,75 @@ package com.example.festpuk.cliente;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.festpuk.R;
+import com.example.festpuk.entity.Cuenta;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ClientePerfilFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ClientePerfilFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ClientePerfilFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ClientePerfilFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ClientePerfilFragment newInstance(String param1, String param2) {
-        ClientePerfilFragment fragment = new ClientePerfilFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    FirebaseAuth mAuth;
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference = firebaseDatabase.getReference();
+    Cuenta cuenta;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cliente_perfil, container, false);
+
+        String keyCuenta = mAuth.getUid();
+
+        View view = inflater.inflate(R.layout.fragment_cliente_perfil, container, false);
+        TextView teNombreApellido = view.findViewById(R.id.teNombreApellido);
+        TextView teFechaNac = view.findViewById(R.id.teFechaNac);
+        TextView tedni = view.findViewById(R.id.teDNI);
+        TextView tecorreo = view.findViewById(R.id.teCorreo);
+        TextView terol = view.findViewById(R.id.teRol);
+
+        databaseReference.child("cuenta/"+keyCuenta).addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot children : snapshot.getChildren()) {
+                    cuenta = children.getValue(Cuenta.class);
+
+                    teNombreApellido.setText(cuenta.getNombre());
+                    teFechaNac.setText(cuenta.getFechanac());
+                    tedni.setText(cuenta.getDni());
+                    tecorreo.setText(cuenta.getCorreo());
+                    terol.setText(cuenta.getRol());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        return view;
     }
 }
